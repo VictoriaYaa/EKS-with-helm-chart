a3eeee545338b4b945bb809918706c9338defd3a
Victoria Yaakov <victoriaja1916@gmail.com>
2024-05-06 12:45:46 +0300

    changed tag

---
+++ np.tf
@@ -0,0 +1,49 @@
+resource "kubernetes_network_policy" "network_policy" {
+  metadata {
+    name      = "terraform-example-network-policy"
+    namespace = kubernetes_namespace.vic-ns.metadata[0].name
+  }
+
+  spec {
+    pod_selector {
+      match_expressions {
+        key      = "app.kubernetes.io/instance"
+        operator = "In"
+        values   = ["httpbin"]
+      }
+    }
+
+    ingress {
+      ports {
+        port     = "http"
+        protocol = "TCP"
+      }
+      ports {
+        port     = "8125"
+        protocol = "UDP"
+      }
+
+      from {
+        namespace_selector {
+          match_labels = {
+            name = "vic-ns"
+          }
+        }
+      }
+
+      from {
+        ip_block {
+          cidr = "0.0.0.0/8"
+          except = [
+            "10.0.0.0/24",
+            "10.0.1.0/24",
+          ]
+        }
+      }
+    }
+
+    egress {} 
+
+    policy_types = ["Ingress", "Egress"]
+  }
+}
